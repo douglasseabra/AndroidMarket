03093644af463e4edb31ed815dbb803382edfd0c
Douglas Seabra <douglas.seabra@pontomobi.com.br>
2016-07-15 23:03:06 -0300

    DEV - Implementation Drawer Layout (lib)           Implement My Profile fragment.

--- app/src/main/java/br/com/market/androidmarket/ui/MainActivity.java
+++ app/src/main/java/br/com/market/androidmarket/ui/MainActivity.java
@@ -1,18 +1,27 @@
 package br.com.market.androidmarket.ui;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.holder.BadgeStyle;
+import com.mikepenz.materialdrawer.holder.StringHolder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import br.com.market.androidmarket.AndroidMarketApplication;
 import br.com.market.androidmarket.R;
+import br.com.market.androidmarket.models.UserAccount;
 import br.com.market.androidmarket.services.AndroidMarketRepository;
 import br.com.market.androidmarket.services.Callback;
 import br.com.market.androidmarket.services.responses.UserAccountResponse;
@@ -21,19 +30,20 @@ import br.com.market.androidmarket.ui.profile.ProfileFragment;
 import br.com.market.androidmarket.ui.sales.SalesFragment;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
-    @Bind(R.id.nav_view)
-    NavigationView navigationView;
+//    @Bind(R.id.nav_view)
+//    NavigationView navigationView;
 
-    @Bind(R.id.drawer_layout)
-    DrawerLayout drawer;
+//    @Bind(R.id.drawer_layout)
+//    DrawerLayout drawer;
+    private TextView badge;
+    private AccountHeader accountHeader;
+    private Drawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,36 +54,84 @@ public class MainActivity extends BaseActivity
 
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(this);
+         badge = (TextView) toolbar.findViewById(R.id.tv_badge);
 
         loadUserAccount();
 
-        onNavigationItemSelected(navigationView.getMenu().getItem(0));
+        initDrawer();
+
+        navigate(1);
     }
 
-    private void loadUserAccount(){
+    private void initDrawer() {
+
+        UserAccount account = AndroidMarketApplication.getInstance().getUserAccountSession();
+
+        IProfile profile = new ProfileDrawerItem()
+                .withName(account.getName())
+                .withEmail(account.getEmail())
+                .withIcon(getResources().getDrawable(account.getPhoto()));
+
+        accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.profile_header)
+                .withSelectionListEnabledForSingleProfile(false)
+                .addProfiles(
+                        profile
+                )
+                .build();
+
+        TextView tvAccountName = (TextView) accountHeader.getView().findViewById(R.id.material_drawer_account_header_name);
+
+
+        TextView tvAccountEmail = (TextView) accountHeader.getView().findViewById(R.id.material_drawer_account_header_email);
+        if (Build.VERSION.SDK_INT < 23) {
+            tvAccountEmail.setTextAppearance(this, R.style.ThemeDrawerHeader_Account_Email);
+            tvAccountName.setTextAppearance(this, R.style.ThemeDrawerHeader_Account_Name);
+        } else {
+            tvAccountEmail.setTextAppearance(R.style.ThemeDrawerHeader_Account_Email);
+            tvAccountName.setTextAppearance(R.style.ThemeDrawerHeader_Account_Name);
+        }
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(accountHeader)
+                .withDisplayBelowStatusBar(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withHasStableIds(true)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        navigate(position);
+                        return false;
+                    }
+                })
+                .build();
+
+
+        BadgeStyle badgeStyle = new BadgeStyle().withBadgeBackground(getResources().getDrawable(R.drawable.bg_badge)).withTextColor(Color.WHITE).withColorRes(R.color.orage_e7ad45);
+
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_sales).withIcon(getResources().getDrawable(R.drawable.ico_minhas_vendas)));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_products).withIcon(getResources().getDrawable(R.drawable.ico_meus_produtos)));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_menbers).withIcon(getResources().getDrawable(R.drawable.ico_afiliados)).withIdentifier(1).withBadgeStyle(badgeStyle));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_messages).withIcon(getResources().getDrawable(R.drawable.ico_mensagens)).withIdentifier(2).withBadgeStyle(badgeStyle));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_notifications).withIcon(getResources().getDrawable(R.drawable.ico_notificacoes)).withIdentifier(3).withBadgeStyle(badgeStyle));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_account).withIcon(getResources().getDrawable(R.drawable.ico_minha_conta)));
+        drawer.addItem(new PrimaryDrawerItem().withName(R.string.menu_about).withIcon(getResources().getDrawable(R.drawable.ico_sobre_o_app)));
+
+        drawer.updateBadge(1, new StringHolder("121"));
+        drawer.updateBadge(2, new StringHolder("+50"));
+        drawer.updateBadge(3, new StringHolder("15"));
+
+    }
+
+    private void loadUserAccount() {
 
         AndroidMarketRepository.with().getUserAccount(new Callback<UserAccountResponse>() {
             @Override
             public void success(UserAccountResponse userResponse) {
                 AndroidMarketApplication.getInstance().setUserAccountSession(userResponse.getUserAccount());
-
-
-                View header = navigationView.getHeaderView(0);
-                TextView txtName = (TextView) header.findViewById(R.id.userName);
-                txtName.setText(userResponse.getUserAccount().getName());
-
-                TextView txtEmail = (TextView) header.findViewById(R.id.userEmail);
-                txtEmail.setText(userResponse.getUserAccount().getEmail());
-
-                CircleImageView imgPhoto = (CircleImageView) header.findViewById(R.id.userPhoto);
-                imgPhoto.setImageResource(userResponse.getUserAccount().getPhoto());
-
             }
 
             @Override
@@ -83,67 +141,58 @@ public class MainActivity extends BaseActivity
         });
     }
 
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
+        if(drawer.isDrawerOpen()){
+            drawer.closeDrawer();
+        }else {
             super.onBackPressed();
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void navigate(int position) {
+        setBadgeToolbar("");
+        switch (position) {
+            case 1:
+                replaceFragment(SalesFragment.newInstance(), R.id.container_fragment, "my_sales_frag", false);
+                changeTitleToolbar(R.string.fragment_sales);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                replaceFragment(MessageFragment.newInstance(), R.id.container_fragment, "message_frag", true);
+                changeTitleToolbar(R.string.fragment_message);
+                setBadgeToolbar("+15");
+                break;
+            case 5:
+                break;
+            case 6:
+                replaceFragment(ProfileFragment.newInstance(), R.id.container_fragment, "my_profile_frag", true);
+                changeTitleToolbar(R.string.fragment_profile);
+                break;
+            case 7:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            replaceFragment(MessageFragment.newInstance(), R.id.container_fragment, "message_fragment", false);
-            changeTitleToolbar(R.string.fragment_message);
-        } else if (id == R.id.nav_gallery) {
-            replaceFragment(SalesFragment.newInstance(), R.id.container_fragment, "message_fragment", false);
-            changeTitleToolbar(R.string.fragment_sales);
-        } else if (id == R.id.nav_slideshow) {
-            replaceFragment(ProfileFragment.newInstance(), R.id.container_fragment, "message_fragment", false);
-            changeTitleToolbar(R.string.fragment_profile);
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void changeTitleToolbar(int title){
+    private void changeTitleToolbar(int title) {
         getSupportActionBar().setTitle(title);
     }
+
+    private void setBadgeToolbar(String value) {
+        if(!TextUtils.isEmpty(value)) {
+            badge.setVisibility(View.VISIBLE);
+            badge.setText(value);
+    }else {
+            badge.setVisibility(View.GONE);
+        }
+
+
+    }
+
 }
--- app/src/main/java/br/com/market/androidmarket/ui/profile/ProfileFragment.java
+++ app/src/main/java/br/com/market/androidmarket/ui/profile/ProfileFragment.java
@@ -8,14 +8,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
 import br.com.market.androidmarket.R;
 import br.com.market.androidmarket.models.Sale;
+import br.com.market.androidmarket.models.User;
 import br.com.market.androidmarket.services.AndroidMarketRepository;
 import br.com.market.androidmarket.services.Callback;
+import br.com.market.androidmarket.services.responses.MessageResponse;
 import br.com.market.androidmarket.services.responses.SaleResponse;
+import br.com.market.androidmarket.ui.message.MessageAdapter;
 import br.com.market.androidmarket.ui.sales.SalesAdapter;
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,9 +32,18 @@ public class ProfileFragment extends Fragment {
     @Bind(R.id.rv_sales)
     RecyclerView mRecyclerView;
 
+    @Bind(R.id.rv_grid_message)
+    RecyclerView messageRecyclerView;
+
+    @Bind(R.id.tv_badge)
+    TextView tvMessageBadge;
+
     private RecyclerView.LayoutManager mLinearLayoutManager;
     private SalesAdapter mSalesAdapter;
+
+    private MessageAdapter mMessageAdapter;
     private List<Sale> mListSales;
+    private List<User> mListContacts;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -40,10 +53,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         loadSales();
+        loadMessages();
     }
 
 
@@ -70,6 +81,20 @@ public class ProfileFragment extends Fragment {
         mSalesAdapter = new SalesAdapter(getContext(), mListSales);
 
         mRecyclerView.setAdapter(mSalesAdapter);
+
+        LinearLayoutManager messageLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        messageRecyclerView.setLayoutManager(messageLayoutManager);
+        mMessageAdapter = new MessageAdapter(getContext(), mListContacts);
+        messageRecyclerView.setAdapter(mMessageAdapter);
+
+        int messageCount = mListContacts.size();
+
+        if(messageCount>4){
+            tvMessageBadge.setText("+" + (messageCount -4));
+        }else{
+            tvMessageBadge.setVisibility(View.INVISIBLE);
+        }
+
     }
 
     private void loadSales(){
@@ -77,6 +102,21 @@ public class ProfileFragment extends Fragment {
             @Override
             public void success(SaleResponse saleResponse) {
                 mListSales = saleResponse.getListSales();
+            }
+
+            @Override
+            public void failure() {
+                //Implements error return
+            }
+        });
+    }
+
+    private void loadMessages() {
+        AndroidMarketRepository.with().getContacts(new Callback<MessageResponse>() {
+            @Override
+            public void success(MessageResponse messageResponse) {
+                mListContacts = messageResponse.getListContacts();
+
             }
 
             @Override
